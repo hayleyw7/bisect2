6b7689216160310301cc1c466b8c56e33d425bb7
Brent Laster <bcl2@nclasters.org>
2021-03-08 22:56:05 -0500

    commit for v1.01

---
+++ sum2.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+ver="1.01"
+echo "Sum 2 program  - version $ver"
+[ -z "$1" ] || [ -z "$2" ] && echo "Not enough arguments supplied" && exit 1
+exprans=`expr $1 + $2`
+echo "$1 + $2 = $exprans"
+exit 0
